a1093d7606b5922a8ba138f36df59509807777a8
Ayisha <ayishasofhia@gmail.com>
2025-05-05 05:53:44 +0800

    POS

---
+++ ADET/A04/shared/data.js
@@ -0,0 +1,414 @@
+var products = [
+    {
+        "category": "Architecture",
+        "contents": [
+            {
+                "isAvailable": true,
+                "name": "Trevi Fountain",
+                "code": "Trevi",
+                "price": 895,
+                "image": "image/architecture/trevi.webp"
+            },
+            {
+                "isAvailable": true,
+                "name": "Notre-Dame de Paris",
+                "code": "Notre",
+                "price": 12880,
+                "image": "image/architecture/notre.webp"
+            },
+            {
+                "isAvailable": true,
+                "name": "Himeji Castle",
+                "code": "Himeji",
+                "price": 8960,
+                "image": "image/architecture/himeji.webp"
+            },
+            {
+                "isAvailable": true,
+                "name": "Great Pyramid of Giza",
+                "code": "Great",
+                "price": 7280,
+                "image": "image/architecture/great.webp"
+            },
+            {
+                "isAvailable": true,
+                "name": "Statue of Liberty",
+                "code": "Statue",
+                "price": 6720,
+                "image": "image/architecture/statue.webp"
+            },
+            {
+                "isAvailable": true,
+                "name": "London",
+                "code": "London",
+                "price": 2240,
+                "image": "image/architecture/london.webp"
+            },
+            {
+                "isAvailable": true,
+                "name": "Paris",
+                "code": "Paris",
+                "price": 2800,
+                "image": "image/architecture/paris.webp"
+            },
+            {
+                "isAvailable": true,
+                "name": "New York City",
+                "code": "NYC",
+                "price": 3360,
+                "image": "image/architecture/new.webp"
+            }
+        ]
+    },
+    {
+        "category": "Botanical Collection",
+        "contents": [
+            {
+                "isAvailable": true,
+                "name": "Petite Sunny Bouquet",
+                "code": "Petite",
+                "price": 1680,
+                "image": "image/botanical collection/petite.webp"
+            },
+            {
+                "isAvailable": true,
+                "name": "Japanese Red Maple Bonsai Tree",
+                "code": "Japanese",
+                "price": 3360,
+                "image": "image/botanical collection/japanese.webp"
+            },
+            {
+                "isAvailable": true,
+                "name": "Flower Arrangement",
+                "code": "Flower",
+                "price": 6160,
+                "image": "image/botanical collection/flower.webp"
+            },
+            {
+                "isAvailable": true,
+                "name": "Pretty Pink Flower Bouquet",
+                "code": "Pretty",
+                "price": 3360,
+                "image": "image/botanical collection/pretty.webp"
+            },
+            {
+                "isAvailable": true,
+                "name": "Mini Orchid",
+                "code": "Mini",
+                "price": 1680,
+                "image": "image/botanical collection/mini.webp"
+            },
+            {
+                "isAvailable": true,
+                "name": "Lucky Bamboo",
+                "code": "Lucky",
+                "price": 1680,
+                "image": "image/botanical collection/lucky.webp"
+            },
+            {
+                "isAvailable": true,
+                "name": "Pulm Blossom",
+                "code": "Pulm",
+                "price": 1680,
+                "image": "image/botanical collection/pulm.webp"
+            },
+            {
+                "isAvailable": true,
+                "name": "Orchid",
+                "code": "Orchid",
+                "price": 2800,
+                "image": "image/botanical collection/orchid.webp"
+            },
+            {
+                "isAvailable": true,
+                "name": "Chrysanthemum",
+                "code": "Chry",
+                "price": 1680,
+                "image": "image/botanical collection/chry.webp"
+            },
+            {
+                "isAvailable": true,
+                "name": "Bonsai Tree",
+                "code": "Bonsai",
+                "price": 2800,
+                "image": "image/botanical collection/bonsai.webp"
+            }
+        ]
+    },
+    {
+        "category": "Bluey",
+        "contents": [
+            {
+                "isAvailable": true,
+                "name": "Bluey's Family House",
+                "code": "Family",
+                "price": 3920,
+                "image": "image/bluey/family.webp"
+            },
+            {
+                "isAvailable": true,
+                "name": "Playground Fun with Bluey and Chloe",
+                "code": "Playground",
+                "price": 1120,
+                "image": "image/bluey/playground.webp"
+            },
+            {
+                "isAvailable": true,
+                "name": "Bluey's Beach and Family Car Trip",
+                "code": "Beach",
+                "price": 1680,
+                "image": "image/bluey/beach.webp"
+            },
+            {
+                "isAvailable": true,
+                "name": "Ice Cream Trip with Bluey ",
+                "code": "Ice Cream",
+                "price": 1680,
+                "image": "image/bluey/ice.webp"
+            },
+            {
+                "isAvailable": true,
+                "name": "Bluey's Family House with Memory Game",
+                "code": "Memory",
+                "price": 3920,
+                "image": "image/bluey/memory.webp"
+            }
+        ]
+    },
+    {
+        "category": "Wicked",
+        "contents": [
+            {
+                "isAvailable": true,
+                "name": "Welcome to Emerald City",
+                "code": "Emerald",
+                "price": 5600,
+                "image": "image/wicked/emerald.webp"
+            },
+            {
+                "isAvailable": true,
+                "name": "Elphaba & Glinda Figures",
+                "code": "Figures",
+                "price": 3080,
+                "image": "image/wicked/figures.webp"
+            },
+            {
+                "isAvailable": true,
+                "name": "Glinda & Elphaba's Dormitory",
+                "code": "Dormitory",
+                "price": 3920,
+                "image": "image/wicked/dormitory.webp"
+            },
+            {
+                "isAvailable": true,
+                "name": "Glinda,Elphaba & Nessarose at Shiz University",
+                "code": "DShiz",
+                "price": 1680,
+                "image": "image/wicked/shiz.webp"
+            }
+        ]
+    },
+    {
+        "category": "Marvel",
+        "contents": [
+            {
+                "isAvailable": true,
+                "name": "Iron Spider-Man Bust",
+                "code": "Bust",
+                "price": 3360,
+                "image": "image/Marvel/bust.webp"
+            },
+            {
+                "isAvailable": true,
+                "name": "Peter Parker's Apartment",
+                "code": "Apartment",
+                "price": 3080,
+                "image": "image/Marvel/apartment.webp"
+            },
+            {
+                "isAvailable": true,
+                "name": "Avengers Tower",
+                "code": "Tower",
+                "price": 28000,
+                "image": "image/Marvel/tower.webp"
+            },
+            {
+                "isAvailable": true,
+                "name": "X-Men: The X-MAnsion",
+                "code": "Mansion",
+                "price": 18480,
+                "image": "image/Marvel/mansion.webp"
+            },
+            {
+                "isAvailable": true,
+                "name": "Daily Bugle",
+                "code": "Bugle",
+                "price": 19600,
+                "image": "image/Marvel/bugle.webp"
+            },
+            {
+                "isAvailable": true,
+                "name": "Infinity Gauntlet",
+                "code": "Gauntlet",
+                "price": 4480,
+                "image": "image/Marvel/gauntlet.webp"
+            },
+            {
+                "isAvailable": true,
+                "name": "Potted Groot",
+                "code": "Groot",
+                "price": 560,
+                "image": "image/Marvel/groot.webp"
+            },
+            {
+                "isAvailable": true,
+                "name": "Guardians of the Galaxy: The Milano",
+                "code": "Milano",
+                "price": 10080,
+                "image": "image/Marvel/milano.webp"
+            },
+            {
+                "isAvailable": true,
+                "name": "Marvel Logo & Minifigures",
+                "code": "Minifigures",
+                "price": 5600,
+                "image": "image/Marvel/minifigures.webp"
+            },
+            {
+                "isAvailable": true,
+                "name": "Iron Man's Laboratory: Hall of Armor",
+                "code": "Laboratory",
+                "price": 3080,
+                "image": "image/Marvel/laboratory.webp"
+            },
+        ]
+    },
+    {
+        "category": "Minecraft",
+        "contents": [
+            {
+                "isAvailable": true,
+                "name": "The Crafting Table",
+                "code": "Crafting",
+                "price": 5040,
+                "image": "image/Minecraft/crafting.webp"
+            },
+            {
+                "isAvailable": true,
+                "name": "Woodland Mansion Fighting Ring",
+                "code": "Woodland",
+                "price": 2800,
+                "image": "image/Minecraft/woodland.webp"
+            },
+            {
+                "isAvailable": true,
+                "name": "The Pillager Outpost and Ravager",
+                "code": "Pillager",
+                "price": 4480,
+                "image": "image/Minecraft/pillager.webp"
+            },
+            {
+                "isAvailable": true,
+                "name": "The Nether Lava Battle",
+                "code": "Lava",
+                "price": 560,
+                "image": "image/Minecraft/lava.webp"
+            },
+            {
+                "isAvailable": true,
+                "name": "The TNT Jungle House",
+                "code": "TNT",
+                "price": 1680,
+                "image": "image/Minecraft/tnt.webp"
+            },
+            {
+                "isAvailable": true,
+                "name": "TThe Illager Desert Patrol",
+                "code": "Illager",
+                "price": 840,
+                "image": "image/Minecraft/illager.webp"
+            },
+            {
+                "isAvailable": true,
+                "name": "The Mooshroom House",
+                "code": "Mooshroom",
+                "price": 2800,
+                "image": "image/Minecraft/mooshroom.webp"
+            },
+            {
+                "isAvailable": true,
+                "name": "The Armadillo Mine Expedition",
+                "code": "Armadillo",
+                "price": 1680,
+                "image": "image/Minecraft/armadillo.webp"
+            },
+            {
+                "isAvailable": true,
+                "name": "The Trial Chamber",
+                "code": "Chamber",
+                "price": 2240,
+                "image": "image/Minecraft/chamber.webp"
+            },
+            {
+                "isAvailable": true,
+                "name": "The Nether Portal Ambush",
+                "code": "Portal",
+                "price": 2120,
+                "image": "image/Minecraft/portal.webp"
+            }
+        ]
+    },
+];
+
+
+var total = 0;
+
+function loadCategories() {
+    var categoriesContainer = document.getElementById("categories");
+    products.forEach((product, index) => {
+        categoriesContainer.innerHTML += `
+<div onclick="loadProducts('${index}')" class="card mx-1 category-button p-3 text-center">
+  <small>${product.category}</small>
+</div>
+`;
+    });
+}
+
+function loadProducts(categoryIndex) {
+    var maincontainer = document.getElementById("maincontainer");
+    maincontainer.innerHTML = "";
+
+    products[categoryIndex].contents.forEach(content => {
+        if (content.sizes) {
+            content.sizes.forEach(size => {
+                maincontainer.innerHTML += `
+                <div onclick="addToReceipt('${size.price}','${content.code + size.code}')" class="card mx-1 my-2 product-button p-3 text-center">
+                    <img src="${content.image}" alt="${content.name}" class="product-img mb-2">
+                    <small>${content.name} ${size.code}</small>
+                </div>
+                `;
+            });
+        } else {
+            maincontainer.innerHTML += `
+            <div onclick="addToReceipt('${content.price}','${content.code}')" class="card mx-1 my-2 product-button p-3 text-center">
+                <img src="${content.image}" class="product-img mb-2">
+                <small>${content.name}</small>
+            </div>
+            `;
+        }
+    });
+}
+
+function addToReceipt(price, code) {
+    var receiptContainer = document.getElementById("receipt");
+    total = parseFloat(total) + parseFloat(price);
+    document.getElementById("totalValue").innerText = total;
+    receiptContainer.innerHTML += `
+<div class="d-flex flex-row justify-content-between">
+<div><small>${code}</small></div>
+<div><small>${price}</small></div>
+</div>
+`;
+}
+
+loadCategories();
